0a39b2c0b01768510b6117c03fba5a3e2b070d66
Even Stensberg <evenstensberg@gmail.com>
2021-04-23 17:55:51 +0200

    chore: wip

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
-const launchHeadless = require('./lib/headless-launcher');
 const chalk = require('chalk');
 const path = require('path');
-const fs = require('fs');
 
 const printHeaderBasedOnWidth = (title) => {
   const half = Math.round(process.stdout.columns / 2);
@@ -19,11 +17,8 @@ module.exports = class NetworkPlugin {
   constructor(options) {
     this.chunkPaths = [];
     this.url = options.url || '';
-    this.flags = options.flags || [];
-    this.headless = (options.headless === false) ? false : true;
-    this.printJSON = (options.printJSON === true) ? true : false;
-    this.printPath = options.printPath || path.join(process.cwd() + '/network-records.json');
   }
+
   apply(compiler) {
     if (this.url.length === 0) {
       if (compiler.options.devServer) {
@@ -48,53 +43,8 @@ module.exports = class NetworkPlugin {
     });
 
     compiler.plugin('done', () => {
-      launchHeadless(this).then(chunkInfo => {
-        process.stdout.write('\n' + chalk.bold('webpack-plugin-extended-network:') + '\n\n');
-        chunkInfo.forEach( (chunk) => {
-          const scriptInjection = Object.keys(chunk.attributes).map( (elem, index) => {
-            if (index % 2 === 0) {
-              return chunk.attributes[index] + '=' + chunk.attributes[index + 1];
-            }
-          }).filter(val => val);
-          process.stdout.write(chalk.red('Asset: ') + chunk.chunkName + '\n\n');
-          printHeaderBasedOnWidth('General');
-          process.stdout.write('\n\n' + chalk.bold('Type: ') +
-            chunk.params.type + '\n' +
-            chalk.bold('Resource URL: ') + chunk.params.response.url + '\n' +
-            chalk.bold('Status: ') + chunk.params.response.status + '\n' +
-            chalk.bold('Status Text: ') + chunk.params.response.statusText + '\n\n'
-          );
-          printHeaderBasedOnWidth('Headers');
-          process.stdout.write('\n\n');
-          Object.keys(chunk.params.response.headers).forEach(header => {
-            process.stdout.write(
-              '\n' + chalk.bold(header + ': ') + chunk.params.response.headers[header] + '\n'
-            );
-          });
-          process.stdout.write('\n');
-          printHeaderBasedOnWidth('Other');
-          process.stdout.write('\n\n');
-          process.stdout.write(
-            chalk.bold('Served from Disk Cache: ') + chunk.params.response.fromDiskCache + '\n' +
-            chalk.bold('Served from Service Worker: ') +
-            chunk.params.response.fromServiceWorker + '\n' +
-            chalk.bold('Protocol: ') + chunk.params.response.protocol + '\n' +
-            chalk.bold('Security State: ') + chunk.params.response.securityState + '\n'
-          );
-          process.stdout.write(chalk.bold('HTML tag: ') + '< ' + chunk.injectionType + ' ');
-          scriptInjection.forEach( (item) => {
-            process.stdout.write(item + ' ');
-          });
-          process.stdout.write('>\n\n');
-          for (let k = 0; k < process.stdout.columns; k++) {
-            process.stdout.write(chalk.blue.bold('#'));
-          }
-          process.stdout.write('\n\n');
-        });
-        if (this.printJSON === true) {
-          fs.writeFileSync(this.printPath, JSON.stringify(chunkInfo, null, 2), 'utf-8');
-        }
-      });
-    });
+      console.log("HEY")
+      require('./test.js')()
+    })
   }
 };
--- test.js
+++ test.js
@@ -1,21 +1,62 @@
 const puppeteer = require('puppeteer');
 
-(async() => {
-  // Use Puppeteer to launch a browser and open a page.
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+(async () => {
+    const browser = await puppeteer.launch();
+    const [page] = await browser.pages();
 
-  // Create a raw DevTools protocol session to talk to the page.
-  // Use CDP to set the animation playback rate.
-  const session = await page.target().createCDPSession();
-  await session.send('Animation.enable');
-  session.on('Animation.animationCreated', () => {
-    console.log('Animation created!');
-  });
-  await session.send('Animation.setPlaybackRate', {
-    playbackRate: 2,
-  });
+    const results = []; // collects all results
 
-  // Check it out! Fast animations on the "loading..." screen!
-  await page.goto('https://pptr.dev');
+    let paused = false;
+    let pausedRequests = [];
+
+    const nextRequest = () => { // continue the next request or "unpause"
+        if (pausedRequests.length === 0) {
+            paused = false;
+        } else {
+            // continue first request in "queue"
+            (pausedRequests.shift())(); // calls the request.continue function
+        }
+    };
+
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        if (paused) {
+            pausedRequests.push(() => request.continue());
+        } else {
+            paused = true; // pause, as we are processing a request now
+            request.continue();
+        }
+    });
+
+    page.on('requestfinished', async (request) => {
+        const response = await request.response();
+
+        const responseHeaders = response.headers();
+        let responseBody;
+        if (request.redirectChain().length === 0) {
+            // body can only be access for non-redirect responses
+            responseBody = await response.buffer();
+        }
+
+        const information = {
+            url: request.url(),
+            requestHeaders: request.headers(),
+            requestPostData: request.postData(),
+            responseHeaders: responseHeaders,
+            responseSize: responseHeaders['content-length'],
+            responseBody,
+        };
+        results.push(information);
+
+        nextRequest(); // continue with next request
+    });
+    page.on('requestfailed', (request) => {
+        // handle failed request
+        nextRequest();
+    });
+
+    await page.goto('http://localhost:8090', { waitUntil: 'networkidle0' });
+    console.log(results);
+
+    await browser.close();
 })();
